40997f941be4892fbf995c0cfa991281b61cd848
Swastik Nath <swastiknath@outlook.com>
2020-05-11 17:25:33 +0530

    fix: Fixed Issues regarding Convergance Issues
    Fixed Isseues regarding the issues in convergance of the DQN Architecture with implementation of Dueling and Double DQN.

--- dqnAgent.py
+++ dqnAgent.py
@@ -1,89 +1,169 @@
 '''
-AUTHOR: SWASTIK NATH.
-
+AUTHOR : SWASTIK NATH
+UDACITY DEEP REINFORCEMENT LEARNING NANODEGREE
+DQN RL AGENT
+IMPLEMENTATION IN COURTESY OF UDACITY.
 '''
 import numpy as np
 import random
-from collections import deque, namedtuple
+from collections import namedtuple, deque
+
+from model import QNetwork
+
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
 BUFFER_SIZE = int(1e5)
 BATCH_SIZE = 64
 GAMMA = 0.99
-TAU = int(1e-3)
 LR = 5e-4
-UPDATE_EVERY=4
+TAU = 1e-3
+UPDATE_EVERY = 4   #FREQUENCY OF LEARNING FROM EXPERIENCE REPLAY BUFFER
 
-device = 'CUDA' if torch.cuda.is_available() else 'CPU'
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-class Agent():
-    
-    def __init__(self, state_size, action_size, seed):
-        self.state_size = state_size
-        self.action_size=action_size
-        self.seed = random.seed(seed)
-        self.qnet_local = QNetwork(state_size, action_size, BATCH_SIZE, BATCH_SIZE, seed).to(device)
-        self.qnet_target = QNetwork(self.state_size, self.action_size, BATCH_SIZE, BATCH_SIZE, seed).to(device)
-        self.optimizer = optim.Adam(self.qnet_local.parameters(), lr=LR)
-        self.memory = ExperienceBuffer(self.action_size, BUFFER_SIZE, BATCH_SIZE)
-        self.time_step = 0
-        
-    def step(self, state, action, reward, next_state, done):
-        self.memory.add(state, action, reward, next_state, done)
-        self.time_step = (self.time_step + 1) % UPDATE_EVERY:
-            experiences = self.memory.sample()
-            self.learn(experiences, GAMMA)
-            
-    def learn(self, experiences, gamma):
-        states, actions, rewards, next_states, dones = experiences
-        qtargets_next = self.qnet_target(next_states).detach().max(1).unsqueeze(1)
-        q_targets = rewards + (gamma * q_targets_next * (1 - dones))
-        q_expected = self.qnet_local(states).gather(1, actions)
-        loss = F.mse_loss(q_expected, q_local)
-        self.optimizer.zero_grad()
-        loss.backward()
-        self.optimizer.step()
-        
-    def act(self, state, epsilon = 0.):
-        state = torch.from_numpy(state).float().unsqueeze(0).to(device)
-        self.qnet_local.eval()
-        with torch.no_grad():
-            action_values = self.qnet_local(state)
-        self.qnet_local.train()
-        if random.random > epsilon:
-            return np.argmax(action_values.cpu().data.numpy())
-        else:
-            return np.random_choice(np.arange(self.action_size))
-        
-    def soft_update(self, local_mode, target_mode, tau):
-        for target_param, local_param in zip(target_mode, local_mode):
-            target_param.data_copy(tau * local_param.data + (1 - tau) * target_param.data) 
-        
-class ExperienceBuffer():
-    
-    def __init__(self, action_size, buffer_size, batch_size, seed):
+class ExperienceReplayBuffer():
+    '''
+    SERVING AS THE EXPERIENCE REPLAY BUFFER OF THE DQN FRAMEWORK
+    '''
+    def __init__(self, action_size, buffer_size, batch_size, seed=0):
+        '''
+        PARAMS:
+        ACTION_SIZE: SIZE OF THE ACTION SPACE OF THE ENVIRONMENT
+        BUFFER_SIZE: SIZE OF THE EXPERIENCE REPLAY BUFFER
+        BATCH_SIZE: SIZE OF THE BATCH 
+        SEED : RANDOM SEED
+        '''
         self.batch_size = batch_size
         self.buffer_size = buffer_size
+        self.action_size = action_size
+        self.memory = deque(maxlen=self.buffer_size)
+        self.experience = namedtuple("Experience", field_names=['state', 'action', 'next_state', 'reward', 'done'])
         self.seed = random.seed(seed)
-        self.action_size= action_size
-        self.experience = namedTuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
-        self.memory = deque(maxlen=buffer_size)
-    
-    def add(self, state, action, reward, next_state, done):
-        e = self.experience(state, action , reward, next_state, done)
+        
+    def add(self, state, action, next_state, reward, done):
+        '''
+        ADDING THE S, A, S', R, DONE TUPLE TO THE EXPERIENCE
+        PARAMS:
+        STATE: STATE OF THE ENVIRONMENT
+        ACTION: ACTION TAKEN BASED ON THE POLICY
+        NEXT_STATE: NEXT STATE FROM THE ACTION
+        REWARD: REWARD PROVIDED FROM THE ENVIRONMENT
+        DONE: TERMINATION SIGNAL OF THE EPISODE
+        '''
+        e = self.experience(state, action, next_state, reward, done)
         self.memory.append(e)
         
     def sample(self):
+        '''
+        RETURNING SAMPLES OF THE S, A, S', R, DONE TUPLES FROM THE MEMORY 
+        TO LEARN FROM THEM LATER.
+        '''
         experiences = random.sample(self.memory, k=self.batch_size)
         states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
         actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).long().to(device)
-        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e in not None])).float().to(device)
+        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(device)
         next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(device)
         dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
-        return (states, actions, rewards, next_states, dones)
-    
-    def memStat(self):
+
+        return (states, actions, next_states, rewards, dones)
+
+        
+    def __len__(self):
+        '''
+        RETURNING LENGTH OF THE MEMORY.
+        '''
         return len(self.memory)
+
+class Agent():
+    
+    def __init__(self, state_size, action_size, seed=0, use_dueling=False, use_double_dqn=False):
+        '''
+        PARAMS:
+        STATE_SIZE: SIZE OF THE STATES OF THE ENVIRONMENT.
+        ACTION_SIZE: SIZE OF THE ACTION SPACE OF THE ENVIRONMENT.
+        SEED: RANDOM SEED.
+        USE_DUELING: DQN DUELING ARCHITECTURE
+        USE_DOUBLE_DQN: USING THE DOUBLE DQN ARCHITECTURE
+        '''
+        self.state_size = state_size
+        self.action_size = action_size
+        self.seed = random.seed(seed)
+        self.use_dueling = use_dueling
+        self.use_double_dqn = use_double_dqn
+        
+        self.qnetwork_local = QNetwork(state_size, action_size, seed, use_dueling=use_dueling)
+        self.qnetwork_target = QNetwork(state_size, action_size, seed, use_dueling=use_dueling)
+        print(f"Local Q Network Configuration: {self.qnetwork_local}")
+        print(f"Target Q Network Configuration: {self.qnetwork_target}")
+        self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=LR)
+        self.memory = ExperienceReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, seed)
+        self.t_step = 0
+        
+    def step(self, state, action, reward, next_state, done):
+        '''
+        ADDING S, A, R, S' DONE TUPLES TO THE EXPERIENCE BUFFER AND LEARNING FROM THEM.
+        PARAMS:
+        STATE: STATE OF THE ENVIRONMENT
+        ACTION: ACTION TAKEN BASED ON THE POLICY
+        NEXT_STATE: NEXT STATE FROM THE ACTION
+        REWARD: REWARD PROVIDED FROM THE ENVIRONMENT
+        DONE: TERMINATION SIGNAL OF THE EPISODE
+        '''
+        self.memory.add(state, action, reward, next_state, done)
+        self.t_step = (self.t_step + 1) % UPDATE_EVERY
+        if self.t_step == 0:
+            if len(self.memory) > BATCH_SIZE:
+                experiences = self.memory.sample()
+                self.learn(experiences, GAMMA)
+                
+    def act(self, state, eps=0.):
+        '''
+        CHOOSING ACTION FOLLOWING THE EPSILON GREEDY POLICY:
+        PARAMS:
+        STATE:STATE OF THE ENVIRONMENT
+        EPS: EPSILON IMPLEMENTED WITH DECAY OVER TIMESTEPS.
+        '''
+        state = torch.from_numpy(state).float().unsqueeze(0).to(device)
+        self.qnetwork_local.eval()
+        with torch.no_grad():
+            action_val = self.qnetwork_local(state)
+        self.qnetwork_local.train()
+        if random.random() > eps:
+            return np.argmax(action_val.cpu().data.numpy())
+        else:
+            return random.choice(np.arange(self.action_size))
+        
+    def learn(self, experience, gamma):
+        '''
+        LEARNING FROM EXPERIENCE REPLAY BUFFER WITH SOFT UPDATES OVER 
+        THE LOCAL AND TARGET QNETWORK. 
+        EXPERIENCE: S, A, R, S' DONE TUPLES SAMPLED FROM THE EXPERIENCE REPLAY BUFFER MEMORY.
+        GAMMA: HYPERPARAM FOR THE DISCOUNT FACTOR.
+        '''
+        states, actions, rewards,next_states, dones = experience
+        if self.use_double_dqn:
+            indices = torch.argmax(self.qnetwork_local(next_states).detach(),1)
+            Q_targets_next = self.qnetwork_target(next_states).detach().gather(1,indices.unsqueeze(1))
+        else:
+            Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
+        Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
+        Q_expected = self.qnetwork_local(states).gather(1, actions)
+        loss = F.mse_loss(Q_expected, Q_targets)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+        self.soft_update(self.qnetwork_local, self.qnetwork_target, TAU)
+        
+    def soft_update(self, local_model, target_model, tau):
+        '''
+        APPLYING SOFT-UPDATE OVER THE TARGET QNETWORK:
+        THETA' = TAU * THETA + (1-TAU)*THETA'
+        PARAMS:
+        LOCAL_MODEL: LOCAL QNETWORK
+        TARGET_MODEL: TARGET QNETWORK
+        '''
+        for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
+            target_param.data.copy_(tau*local_param.data + (1.0 - tau) * target_param.data)
+            
--- model.py
+++ model.py
@@ -1,23 +1,43 @@
+'''
+AUTHOR : SWASTIK NATH
+UDACITY DEEP REINFORCEMENT LEARNING NANODEGREE
+DQN Q NETWORK 
+IMPLEMENTATION IN COURTESY OF UDACITY.
+'''
 import torch
-import torch.functional as F
 import torch.nn as nn
-import random
+import torch.nn.functional as F
+import torch.optim as optim
 
-class QNetwork():
+class QNetwork(nn.Module):
     '''
-    Creating a Feed Forward Linear Network for processing the states only.
+    QNETWORK TO MAP STATES TO ACTIONS WITH FEEDFORWARD AND DUELING. 
     '''
-    
-    def __init__(self, state_size, action_size, f1_size, f2_size, seed=0):
-        self.seed = random.seed(seed)
-        self.state_size = state_size
-        self.action_size = action_size
-        self.fc1 = nn.Linear(state_size, f1_size)
-        self.fc2 = nn.Linear(f1_size, f2_size)
-        self.fc3 = nn.Linear(f2_size, action_size)
-        
+    def __init__(self, state_size, action_size, seed=5, use_dueling=False, fc1_size=64, fc2_size=64):
+        '''
+        PARAM:
+        STATE_SIZE: SIZE OF THE STATE SPACE OF THE ENVIRONMENT
+        ACTION_SIZE:SIZE OF THE ACTION SPACE OF THE ENVIRONMENT
+        SEED: RANDOM SEED 
+        USE_DUELING: DUELING ARCHITECTURE OF THE DQN
+        FC1_SIZE: SIZE OF FC LAYER 1
+        FC2_SIZE: SIZE OF FC LAYER 2
+        '''
+        super(QNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.dueling = use_dueling
+        self.fc1 = nn.Linear(state_size, fc1_size)
+        self.fc2 = nn.Linear(fc1_size, fc2_size)
+        self.fc3 = nn.Linear(fc2_size, action_size)
+        self.a_state = nn.Linear(fc2_size, 1)
     def forward(self, state):
+        '''
+        PARAM:
+        STATE: STATE VALUES IN DEVICE SPECIFIC TORCH TENSOR
+        '''
         x = F.relu(self.fc1(state))
         x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+        if self.dueling:
+            return self.fc3(x) + self.a_state(x)
+        else:
+            return self.fc3(x)        
